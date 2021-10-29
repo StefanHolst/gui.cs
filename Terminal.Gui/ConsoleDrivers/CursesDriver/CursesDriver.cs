296475bc1433a17d833fa5adcf6583eab0dcf301
Stefan Holst <stefanholst0@gmail.com>
2021-10-29 18:41:31 +0200

    Added support for changing esc button delay in curses.

--- Terminal.Gui/ConsoleDrivers/CursesDriver/CursesDriver.cs
+++ Terminal.Gui/ConsoleDrivers/CursesDriver/CursesDriver.cs
@@ -141,6 +141,10 @@ namespace Terminal.Gui {
 
 			SetCursorVisibility (CursorVisibility.Default);
 
+			// Reset default esc delay before quitting
+			if (CursesDefaultEscDelay != -1)
+				Curses.set_escdelay (CursesDefaultEscDelay);
+			
 			Curses.endwin ();
 
 			// I'm commenting this because was used in a trying to fix the Linux hanging and forgot to exclude it.
@@ -663,7 +667,7 @@ namespace Terminal.Gui {
 
 			// Special handling for ESC, we want to try to catch ESC+letter to simulate alt-letter as well as Alt-Fkey
 			if (wch == 27) {
-				Curses.timeout (200);
+				Curses.timeout (10); // We should really not have a delay here, the experience is annoying.
 
 				code = Curses.get_wch (out int wch2);
 
@@ -788,6 +792,7 @@ namespace Terminal.Gui {
 
 		Action<KeyEvent> keyHandler;
 		Action<MouseEvent> mouseHandler;
+		int CursesDefaultEscDelay = -1;
 
 		public override void PrepareToRun (MainLoop mainLoop, Action<KeyEvent> keyHandler, Action<KeyEvent> keyDownHandler, Action<KeyEvent> keyUpHandler, Action<MouseEvent> mouseHandler)
 		{
@@ -862,6 +867,10 @@ namespace Terminal.Gui {
 
 			Curses.raw ();
 			Curses.noecho ();
+			
+			// Get the expected delay for esc button and store it for resetting.
+			CursesDefaultEscDelay = Curses.get_escdelay ();
+			Curses.set_escdelay (10);
 
 			Curses.Window.Standard.keypad (true);
 			reportableMouseEvents = Curses.mousemask (Curses.Event.AllEvents | Curses.Event.ReportMousePosition, out oldMouseEvents);
